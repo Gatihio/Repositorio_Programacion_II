7c8d47bb8923c98d695cd8fc4d80918191e9962a
Gatihio <genaroceballos04@gmail.com>
2024-05-08 01:11:38 -0300

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+Numero1 = float(input("Ingrese el primer numero: "))
+Numero2 = float(input("Ingrese el segundo numero"))
+Numero3 = float(input("Ingrese el tercer numero: "))
+
+promedio = (Numero1 + Numero2 + Numero3) / 3
+
+print  ("El promedio de", Numero1, "y", Numero2, "y", Numero3 , "es de:", promedio)
