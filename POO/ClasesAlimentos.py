407f214aebb4f68aa86625bd95ed755c7d9482ad
Gatihio <genaroceballos04@gmail.com>
2024-06-18 11:05:35 -0300

    .

---
+++ POO/ClasesAlimentos.py
@@ -0,0 +1,10 @@
+from ClasesProductos import Producto
+
+class Alimento(Producto):
+    def __init__(self, nombre, precio, cantidad, fecha_expiracion):
+        super().__init__(nombre, precio, cantidad)
+        self.fecha_expiracion = fecha_expiracion
+        
+    def informacion(self):
+        super().informacion()
+        print("Fecha de expiración:", self.fecha_expiracion)
