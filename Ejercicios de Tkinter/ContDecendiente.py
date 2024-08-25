707a53f0510d29b385ede959dcbdca01b086d893
Gatihio <genaroceballos04@gmail.com>
2024-08-25 19:58:32 -0300

    .

---
+++ Ejercicios de Tkinter/ContDecendiente.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+# Función para incrementar el contador y actualizar la etiqueta
+def disminuir_contador():
+    global contador
+    contador -= 1
+    etiqueta_contador.config(text=f"Contador: {contador}")
+
+root = tk.Tk()
+root.title("Ejemplo de marco en Tkinter")
+
+# Crear un marco
+marco = tk.Frame(root, bg="lightblue", padx=10, pady=10)
+marco.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
+
+contador = 88
+
+# Crear una etiqueta para mostrar el contador
+etiqueta_contador = tk.Label(marco, text=f"Contador: {contador}", font=("Arial", 16), bg="lightblue")
+etiqueta_contador.pack(pady=10)
+
+# Agregar un nombre o título al marco usando una etiqueta
+nombre_marcado = tk.Label(marco, text="Contador Decreciente", bg="lightblue", font=("Arial", 14, "bold"))
+nombre_marcado.pack(pady=(0, 10))
+
+# Crear un botón con el símbolo + y una acción
+boton_mas = tk.Button(marco, text="-", font=("Arial", 24), width=5, height=2, command=disminuir_contador)
+boton_mas.pack(pady=10)
+
+# Agregar un widget dentro del marco
+etiqueta = tk.Label(marco, text="¡Hata cuanto podes llegar!", bg="lightblue")
+etiqueta.pack()
+
+# Ejecutar el bucle principal
+root.mainloop()
