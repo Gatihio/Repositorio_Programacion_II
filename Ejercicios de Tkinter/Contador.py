f1dba4acf145b3f85432734e4a1ee905180f80ee
Gatihio <genaroceballos04@gmail.com>
2024-09-08 22:35:37 -0300

    .

--- Ejercicios de Tkinter/Contador.py
+++ Ejercicios de Tkinter/Contador.py
@@ -1,43 +1,39 @@
 import tkinter as tk
 
-def incrementar_contador():
-    global contador
-    contador += 1
-    etiqueta_contador.config(text=f"Contador: {contador}")
+def Count_up():
+    current_value = int(contador.get())
+    contador.set(str(current_value + 1))
+
+def Count_down():
+    current_value = int(contador.get())
+    if current_value > 0:
+        contador.set(str(current_value - 1))
+
+def Count_reset():
+    contador.set("0")
 
 root = tk.Tk()
 root.title("Contador")
 
-contador = 0
+root = tk.Frame(root, bg="lightblue", padx=10, pady=10)
+root.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
 
-etiqueta_contador = tk.Label(root, text=f"Contador: {contador}", font=("Arial", 16), bg="lightblue")
+etiqueta_contador = tk.Label(root, text="Contador")
 etiqueta_contador.pack(pady=10)
 
-marco = tk.Frame(root, bg="lightblue", padx=10, pady=10)
-marco.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
+contador = tk.StringVar()
+contador.set("0")
 
-nombre_marcado = tk.Label(marco, text="Contador", bg="lightblue", font=("Arial", 14, "bold"))
-nombre_marcado.pack(pady=(0, 10))
+entrada_contador = tk.Entry(root, textvariable=contador, state='readonly', font=('Arial', 14))
+entrada_contador.pack(pady=10)
 
-# Crear las etiquetas
-etiqueta_contup = tk.Label(root, text="ContUp")
-etiqueta_contdown = tk.Label(root, text="ContDown")
-etiqueta_reset = tk.Label(root, text="Reset")
+boton_Cont_up = tk.Button(root, text="Count Up", font=("Arial", 15), width=5, height=2, command=Count_up)
+boton_Cont_up.pack(side=tk.LEFT, padx=5,pady=10)
 
-etiqueta_contup.grid(row=0, column=0, padx=10, pady=5, sticky='e')
-etiqueta_contdown.grid(row=1, column=0, padx=10, pady=5, sticky='e')
-etiqueta_reset.grid(row=2, column=0, padx=10, pady=5, sticky='e')
+boton_Cont_down = tk.Button(root, text="Count Down", font=("Arial", 15), width=5, height=2, command=Count_down)
+boton_Cont_down.pack(side=tk.LEFT, padx=5,pady=10)
 
-entrada_contup = tk.Entry(marco, state='readonly') 
-entrada_contdown = tk.Entry(marco, state='readonly') 
-entrada_reset = tk.Entry(marco, state='readonly') 
-
-etiqueta_contup.grid(row=0, column=0, padx=10, pady=5, sticky='e')
-entrada_contup.grid(row=0, column=1, padx=10, pady=5)
-etiqueta_contdown.grid(row=1, column=0, padx=10, pady=5, sticky='e')
-entrada_contdown.grid(row=1, column=1, padx=10, pady=5)
-etiqueta_reset.grid(row=2, column=0, padx=10, pady=5, sticky='e')
-entrada_reset.grid(row=2, column=1, padx=10, pady=5)
-
+boton_reset = tk.Button(root, text="Reset", font=("Arial", 15), width=5, height=2, command=Count_reset)
+boton_reset.pack(side=tk.LEFT, padx=5,pady=10)
 
 root.mainloop()
